6e6f99657eae112eeac155e3425b0c485c15ef16
POBnH4 <bonchp4@yahoo.com>
2018-10-15 13:54:41 +0100

    search page and server.js update

---
+++ server.js
@@ -0,0 +1,24 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/munros";
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const app = express();
+//npm install
+
+var db;
+
+app.use(session({ secret: 'example'}));
+app.use(bodyParser.urlencoded({ extended: true}));
+//app.set('view engine', 'ejs');
+
+MongoClient.connect(url, function(err, database) {
+    if (err) throw err;
+    db = database;
+    app.listen(8080);
+    console.log('listening on 8080');
+});
+
+app.get('/', function(req,res) {
+    res.render('website/html');
+});
