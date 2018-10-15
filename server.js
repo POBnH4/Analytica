a4da4a58da9d3eb534143b1ccdd464265e31e0f0
POBnH4 <bonchp4@yahoo.com>
2018-10-15 16:51:45 +0100

    ejs, server update

--- server.js
+++ server.js
@@ -11,11 +11,15 @@ var db;
 app.use(express.static("public"));
 app.use(session({ secret: 'example'}));
 app.use(bodyParser.urlencoded({ extended: true}));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 MongoClient.connect(url, function(err, database) {
     if (err) throw err;
     db = database;
     app.listen(8080);
     console.log('listening on 8080');
+});
+
+app.get('/', function(req,res) {
+  res.render('/website.ejs')
 });
