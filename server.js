819163b813a272b9a933b1a71a9303d3a209f73a
jozi99 <jozicricekt@hotmail.co.uk>
2018-10-22 21:16:55 +0100

    fff

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.post('/dologin', function(req, res) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){
+      console.log("Error");
         //..console.log('1');
           res.render('pages/website');return}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
