906e382d80681d60ea6eea784a958ac4c5922988
SatyaTamalampudi <tetalasatya123@gmail.com>
2018-10-23 11:45:29 +0100

    fixed database name

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/Analytica_Users";
+const url = "mongodb://localhost:27017/Analytica";
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
