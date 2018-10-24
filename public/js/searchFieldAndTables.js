2ee84fbe522d8838f2c3ac7b470cab462f1e4c5c
jozi99 <jozicricekt@hotmail.co.uk>
2018-10-24 14:07:26 +0100

    g

--- public/js/searchFieldAndTables.js
+++ public/js/searchFieldAndTables.js
@@ -11,10 +11,10 @@
         url: "json/keys.json",
         success: function(result)
         {
-          $scope.keys = result.keys;
+
           //console.log(result.keys[0]);
           for(var i = 0;i<result.keys.length;i++){
-
+                $scope.keys = result.keys[i];
             if(result.keys[i].keyType==("Tambour Unit")){
                 //  $scope.keys = result.keys[0];
 
