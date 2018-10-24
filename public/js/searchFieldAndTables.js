85dafcb8d6938707344f4ae9761d5ff1d99564a4
jozi99 <jozicricekt@hotmail.co.uk>
2018-10-24 14:33:00 +0100

    dd

--- public/js/searchFieldAndTables.js
+++ public/js/searchFieldAndTables.js
@@ -13,17 +13,19 @@
         {
 
           //console.log(result.keys[0]);
-          for(var i = 0;i<result.keys.length;i++){
+          // for(var i = 0;i<result.keys.length;i++){
+          //
+          //   if(result.keys[i].keyType==("Tambour Unit")){
+          //     console.log(result.keys[i])
+          //          $scope.keys = result.keys[i];
+          //
+          //   }
+          //
+          // }
 
-            if(result.keys[i].keyType==("Tambour Unit")){
-              console.log(result.keys[i])
-                   $scope.keys = result.keys[i];
+           $scope.products = [{ id: 1, product: 'Sugar', quality: 'Good', quantity: '200 packs' }, { id: 2, product: 'Wheat', quality: 'Super', quantity: '100 bags' }, { id: 3, product: 'Rice', quality: 'Fine', quantity: '50 packs' }];
 
-            }
-
-          }
-
-            console.log($scope.keys);
+        
 
             }
         })
