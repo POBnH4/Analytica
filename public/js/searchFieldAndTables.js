7807860acc881554c74c0780eddb29a5c14ad688
jozi99 <jozicricekt@hotmail.co.uk>
2018-11-10 19:47:18 +0000

    dd

--- public/js/searchFieldAndTables.js
+++ public/js/searchFieldAndTables.js
@@ -424,6 +424,7 @@
               if(results[i].keyType==="Tambour Unit"){
 
                 $scope.results.push(results[i])
+                console.log($scope.result[i])
 
                 console.log("hello");
               }
