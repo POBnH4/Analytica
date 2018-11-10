a51b65736ebcdc47a37175816c53f2dda6c3c3cd
jozi99 <jozicricekt@hotmail.co.uk>
2018-11-10 23:42:27 +0000

    dd

--- public/js/searchFieldAndTables.js
+++ public/js/searchFieldAndTables.js
@@ -2,10 +2,12 @@
        var app = angular.module('myApp', []);
        app.controller('customersCtrl', function($scope) {
 
-          
+
           //  var results = []
 
+            $(document).ready(function(){
 
+            
 
          var results = [];
 
@@ -425,66 +427,10 @@
               }
            }
 
+})
 
-
-
-
-
-
-
-
-    // $.ajax({
-    //     type: "GET",
-    //     url: "json/keys.json",
-    //     success: function(result)
-    //     {
-    //
-    //         $scope.results = result
-    //         console.log($scope.results)
-    //        for(var i = 0;i<result.keys.length;i++){
-    //
-    //         if(result.keys[i].keyType==("Tambour Unit")){
-    //           $scope.results = result[i]
-    //           console.log($scope.results);
-    //         }
-    //
-    //       }
-    //      $scope.result = results
-    //         console.log($scope.result)
-    //
-    //         }
-    //
-    //
-    //     })
 
 
 
 
   })
-
-
-
-
-
-// function searchFieldFunction() {
-//     var input, filter, ul, li, a, i;
-//     input = document.getElementById("searchField");
-//     filter = input.value.toUpperCase();
-//     ul = document.getElementById("myTR");
-//     li = ul.getElementsByTagName("li");
-//     for (i = 0; i < li.length; i++) {
-//         a = li[i].getElementsByTagName("a")[0];
-//         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//             li[i].style.display = "";
-//         } else {
-//             li[i].style.display = "none";
-//         }
-//     }
-// }
-
-// <tr>
-//   <th scope="row">1</th>
-//   <td>Mark</td>
-//   <td>Otto</td>
-//   <td>@mdo</td>
-// </tr>
