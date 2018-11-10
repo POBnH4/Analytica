e235b215be966e5e4d040ba5adcb24334b121457
jozi99 <jozicricekt@hotmail.co.uk>
2018-11-10 21:17:55 +0000

    ff

--- public/js/searchFieldAndTables.js
+++ public/js/searchFieldAndTables.js
@@ -2,7 +2,7 @@
        var app = angular.module('myApp', []);
        app.controller('customersCtrl', function($scope) {
 
-            $scope.searchText = "";
+          
           //  var results = []
 
 
